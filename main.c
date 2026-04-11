34c30143becdde57fb1017d76d8bffd2606428a3
28112811 <jorgeperalta12g@gmail.com>
2026-04-11 19:56:06 -0400

    Primer case hecho, falta pasarlo a una funcion

--- main.c
+++ main.c
@@ -7,7 +7,6 @@
 #define VENDIDO 1
 #define NO_VENDIDO 0
 
-
 void limpiarBufferFunc(){
     int ch;
     while ((ch = getchar()) != '\n' && ch != EOF);
@@ -34,7 +33,7 @@ int main() {
     int opcion;
 
     do{
-
+        
         printf("\n**************** PUComunicaciones, SRL ****************\n\n");
         printf("1. Registrar Celular\n");
         printf("2. Vender Celular\n");
@@ -49,8 +48,6 @@ int main() {
             limpiarBufferFunc();
             printf("\nOpci%cn no valida, intente de nuevo: ", 162);
         }
-
-        
 
         system("cls");
 
@@ -69,25 +66,68 @@ int main() {
                 }
                 printf("\n************ R E G I S T R O  C E L U L A R E S ************\n");
 
-                for(int i = 0; i < n; i++){
+                char continuar = 'S';
+                while(continuar == 'S' || continuar == 's'){
 
-                    printf("\nIntroducir los datos del celular N0.%d\n",i + 1);
-                    printf("ID: \n");
-                    //scanf();
-                    printf("Marca: \n");
-                    //scanf();
-                    printf("Almacenamiento(GB): \n");
-                    //scanf();
-                    printf("Memoria RAM(GB): \n");
-                    //scanf();
-                    printf("Precio de Venta: \n");
-                    //scanf();
+                    if(totalRegistrados >= n){
 
-                    printf("\nDesea registrar otro celular(S/N)?: \n");
+                        system("cls");
+                        printf("\nCapacidad maxima de celulares alcanzada.\n");
+                        break;
+                    }
 
+                    printf("\nIntroducir los datos del celular No.%d\n", totalRegistrados + 1);
+                    
+                    printf("ID: ");
+                    while(scanf("%d", &idsCelulares[totalRegistrados]) != 1 || idsCelulares[totalRegistrados] <= 0){
 
+                        limpiarBufferFunc();
+                        printf("\nID no valido: ");
+                    }
+                    
+                    printf("Marca: ");
+                    limpiarBufferFunc();
+                    gets(marcas[totalRegistrados]);
+                    
+                    printf("Almacenamiento(GB): ");
+                    while(scanf("%f", &almacenamiento[totalRegistrados]) != 1 || almacenamiento[totalRegistrados] <= 0){
+
+                        limpiarBufferFunc();
+                        printf("\nAlmacenamiento no valido: ");
+                    }
+                    
+                    printf("Memoria RAM(GB): ");
+                    while(scanf("%f", &ram[totalRegistrados]) != 1 || ram[totalRegistrados] <= 0){
+
+                        limpiarBufferFunc();
+                        printf("\nRAM no valida: ");
+                    }
+                    
+                    printf("Precio de Venta: ");
+                    while(scanf("%f", &precios[totalRegistrados]) != 1 || precios[totalRegistrados] <= 0){
+
+                        limpiarBufferFunc();
+                        printf("\nPrecio no valido: ");
+                    }
+                    
+                    vendido[totalRegistrados] = NO_VENDIDO;
+                    totalRegistrados++;
+                    
+                    printf("\nDesea registrar otro celular(S/N)?: ");
+                    while(scanf(" %c", &continuar) != 1 || (continuar != 's' && continuar != 'n' && continuar != 'S' && continuar != 'N')){
+
+                        limpiarBufferFunc();
+                        printf("\nDato Inv%clido, intente de nuevo: ", 160);
+
+                    }
+
+                    if(continuar == 'n' || continuar == 'N'){
+                        
+                        system("cls");
+                        break;
+                    }
+            
                 }
-
                 break;
 
             case 2:
