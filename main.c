38a52a249d6bf26fc06be0bb24926af7ac4b39b8
binformatik007 <binformatik007@gmail.com>
2026-04-11 17:11:36 -0400

    tollo

--- main.c
+++ main.c
@@ -2,7 +2,25 @@
 
 int main(){
     printf("Hola mundo!");
+    printf("Hola mundo!");
+    printf("Hola mundo!");
+    printf("Hola mundo!");
+    printf("Hola mundo!");
+    printf("Hola mundo!");
+    printf("Hola mundo!");
+    printf("Hola mundo!");
+    printf("Hola mundo!");
+    printf("Hola mundo!");
+    printf("Hola mundo!");
+    printf("Hola mundo!");
+    printf("Hola mundo!");
+    printf("Hola mundo!");
+    printf("Hola mundo!");
+    printf("Hola mundo!");
+    printf("Hola mundo!");
+    printf("Hola mundo!");
     
 
+    
     return 0;
 }
